a71fd88d53319f4ba24786750f87d2e2b89a3594
SUDPro <stepan.kurin@yandex.ru>
2019-09-19 12:48:04 +0300

    add parsing html with jsoup. In this version we can read all types of messages and parse it to text if it's html instead of "multitype/alternative" and can't save files from mail.

--- src/main/java/ru/tts/entities/Mail.java
+++ src/main/java/ru/tts/entities/Mail.java
@@ -1,14 +1,13 @@
 package ru.tts.entities;
 
+import com.sun.javafx.css.CssError;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -19,11 +18,24 @@ import java.util.Date;
 public class Mail {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Type(type="text")
     String subject;
+
+    @Type(type="text")
     String receiver;
+
+    @Type(type="text")
     String sender;
 
     @Temporal(TemporalType.DATE)
     Date send_date;
+
+    @Type(type="text")
+    String messageContent;
+
+    @Type(type = "text")
+    String contentType;
 }
--- src/main/java/ru/tts/reader/ReadMail.java
+++ src/main/java/ru/tts/reader/ReadMail.java
@@ -9,13 +9,14 @@ import ru.tts.repositories.MailRepository;
 
 
 import javax.mail.*;
+import javax.mail.internet.ContentType;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMultipart;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.logging.Logger;
 
 @Service
@@ -27,73 +28,145 @@ public class ReadMail {
     private static final String IMAP_AUTH_PWD = "";
     private static final String IMAP_Server = "";
     private static final String IMAP_Port = "993";
+    private static final String saveDirectory = "";
 
     List<String> emails = new ArrayList<String>(Arrays.asList(IMAP_AUTH_EMAIL, IMAP_AUTH_EMAIL1, IMAP_AUTH_EMAIL2));
 
     @Autowired
     MailRepository repository;
 
-    @Scheduled(cron = "0 0/5 * * * ?")
+    @Scheduled(cron = "0 0/1 * * * ?")
     public void readMailFromEmails() {
+
+        for (String email :
+                emails) {
+            downloadEmails();
+        }
+
+    }
+
+    public void downloadEmails() {
         Properties properties = new Properties();
         properties.put("mail.debug", "false");
         properties.put("mail.store.protocol", "imaps");
         properties.put("mail.imap.ssl.enable", "true");
         properties.put("mail.imap.port", IMAP_Port);
+        Session session = Session.getDefaultInstance(properties);
 
-        for (String email :
-                emails) {
-            readingAndSave(properties);
-        }
-
-    }
-
-    public void readingAndSave(Properties properties) {
-        Authenticator auth = new EmailAuthenticator(IMAP_AUTH_EMAIL,
-                IMAP_AUTH_PWD);
-        Session session = Session.getDefaultInstance(properties, auth);
-        session.setDebug(false);
         try {
+            // connects to the message store
             Store store = session.getStore();
-
-            // Подключение к почтовому серверу
             store.connect(IMAP_Server, IMAP_AUTH_EMAIL, IMAP_AUTH_PWD);
 
-            // Папка входящих сообщений
-            Folder inbox = store.getFolder("INBOX");
+            // opens the inbox folder
+            Folder folderInbox = store.getFolder("INBOX");
+            folderInbox.open(Folder.READ_ONLY);
 
-            // Открываем папку в режиме чтения и изменения
-            inbox.open(Folder.READ_WRITE);
-            if (inbox.getMessageCount() == 0) {
-                return;
-            }
-            for (int i = 1; i < inbox.getMessageCount(); i++) {
-                Message message = inbox.getMessage(i);
-                Mail mail = Mail.builder()
-                        .receiver(message.getSubject())
-                        .sender(Arrays.toString(message.getFrom()))
-                        .subject(message.getSubject())
-                        .send_date(message.getSentDate())
-                        .build();
-                repository.save(mail);
-                Multipart mp = (Multipart) message.getContent();
-                for (int j = 0; i < mp.getCount(); i++) {
-                    BodyPart bp = mp.getBodyPart(i);
-                    if (bp.getFileName() == null)
-                        System.out.println("    " + i + ". сообщение : '" +
-                                bp.getContent() + "'");
-                    else
-                        System.out.println("    " + i + ". файл : '" +
-                                bp.getFileName() + "'");
+            // fetches new messages from server
+            Message[] arrayMessages = folderInbox.getMessages();
+            for (Message message :
+                    arrayMessages) {
+                String from = ((message.getFrom())[0]).toString();;
+                String subject = message.getSubject();
+                String toList = parseAddresses(message.getRecipients(Message.RecipientType.TO));
+                Date sentDate = message.getSentDate();
+                String contentType = message.getContentType();
+                String messageContent = "";
+                if (message.isMimeType("text/html")){
+                    messageContent = org.jsoup.Jsoup.parse(message.getContent().toString()).text();
+                } else {
+                    messageContent = getTextFromMessage(message);
                 }
+
+                repository.save(Mail.builder()
+                        .sender(from)
+                        .subject(subject)
+                        .send_date(sentDate)
+                        .receiver(toList)
+                        .messageContent(messageContent)
+                        .contentType(contentType)
+                        .build());
+                System.out.println("\t From: " + from);
+                System.out.println("\t Content-Type: " + contentType);
+                System.out.println("\t To: " + toList);
+                System.out.println("\t Subject: " + subject);
+                System.out.println("\t Sent Date: " + sentDate);
+                System.out.println("\t Message: " + messageContent);
+
             }
-        } catch (NoSuchProviderException e) {
-            System.err.println(e.getMessage());
-        } catch (MessagingException e) {
-            System.err.println(e.getMessage());
-        } catch (IOException e) {
+            // disconnect
+            folderInbox.close(false);
+            store.close();
+        } catch (NoSuchProviderException ex) {
+            System.out.println("No provider for imap");
+            ex.printStackTrace();
+        } catch (MessagingException ex) {
+            System.out.println("Could not connect to the message store");
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
-            System.err.println(e.getMessage());
         }
+    }
+
+    private String getTextFromMessage(Message message) throws IOException, MessagingException {
+        String result = "";
+        if (message.isMimeType("text/plain")) {
+            result = message.getContent().toString();
+        } else if (message.isMimeType("multipart/*")) {
+            MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+            result = getTextFromMimeMultipart(mimeMultipart);
+        }
+        return result;
+    }
+
+    private String getTextFromMimeMultipart(
+            MimeMultipart mimeMultipart) throws IOException, MessagingException {
+
+        int count = mimeMultipart.getCount();
+        if (count == 0)
+            throw new MessagingException("Multipart with no body parts not supported.");
+        boolean multipartAlt = new ContentType(mimeMultipart.getContentType()).match("multipart/alternative");
+        if (multipartAlt)
+            // alternatives appear in an order of increasing
+            // faithfulness to the original content. Customize as req'd.
+            return getTextFromBodyPart(mimeMultipart.getBodyPart(count - 1));
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            result += getTextFromBodyPart(bodyPart);
+        }
+        return result;
+    }
+
+    private String getTextFromBodyPart(
+            BodyPart bodyPart) throws IOException, MessagingException {
+
+        String result = "";
+        if (bodyPart.isMimeType("text/plain")) {
+            result = (String) bodyPart.getContent();
+        } else if (bodyPart.isMimeType("text/html")) {
+            String html = (String) bodyPart.getContent();
+            result = org.jsoup.Jsoup.parse(html).text();
+        } else if (bodyPart.getContent() instanceof MimeMultipart) {
+            result = getTextFromMimeMultipart((MimeMultipart) bodyPart.getContent());
+        }
+        return result;
+    }
+
+    private String parseAddresses(Address[] address) {
+        String listAddress = "";
+
+        if (address != null) {
+            for (int i = 0; i < address.length; i++) {
+                listAddress += address[i].toString() + ", ";
+            }
+        }
+        if (listAddress.length() > 1) {
+            listAddress = listAddress.substring(0, listAddress.length() - 2);
+        }
+
+        return listAddress;
     }
 }
